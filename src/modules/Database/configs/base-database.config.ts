19ecf750966103d2bf866002e232826d0d7ab103
oleksandr-chumak <oleksandr.chumak.anatoliiovych@gmail.com>
2024-05-29 19:34:30 +0200

    feat: Add SnakeNamingStrategy to database config

--- src/modules/Database/configs/base-database.config.ts
+++ src/modules/Database/configs/base-database.config.ts
@@ -1,7 +1,9 @@
 import { DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export const BASE_DATABASE_CONFIG: Partial<DataSourceOptions> = {
   migrations: ['migrations/*.js'],
   logging: ['error'],
   synchronize: false,
+  namingStrategy: new SnakeNamingStrategy(),
 };
